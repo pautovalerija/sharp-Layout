3ecc39a1eef7d7ebd4a7beb8cf04b21b0784eeb5
Opilane TTHK <opilane@tthk.ee>
2019-10-17 15:46:14 +0300

    Dodelat

---
+++ LayoutPautova2/LayoutPautova2/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutPautova2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        Grid grid;
+        private Button btn1;
+        //private Button btn2;
+        //private Button btn3;
+        public MainPage()
+        {
+            grid = new Grid
+            {
+                RowDefinitions =
+                {
+                new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                new RowDefinition{Height=new GridLength(1,GridUnitType.Star)}
+                },
+                ColumnDefinitions =
+                {
+                new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)}
+                }
+            };
+            Button btn1 = new Button();
+            //Button btn2 = new Button();
+            //Button btn3 = new Button();
+            btn1.BackgroundColor = Color.Yellow;
+            //btn2.BackgroundColor = Color.Yellow;
+            //btn3.BackgroundColor = Color.Yellow;
+            grid.Children.Add(btn1, 0, 0);
+           //grid.Children.Add(btn2, 0, 1);
+            //grid.Children.Add(btn3, 0, 2);
+
+            btn1.Clicked += Btn1_Clicked;
+            //btn2.Clicked += Btn2_Clicked;
+            //btn3.Clicked += Btn3_Clicked;
+
+            Content = grid;
+        }
+
+        //private async void Btn3_Clicked(object sender, EventArgs e)
+        //{
+           //await Navigation.PushAsync(new Kolmapäev());
+        //}
+
+        //private async void Btn2_Clicked(object sender, EventArgs e)
+        //{
+           //await Navigation.PushAsync(new Teisipäev());
+        //}
+
+        private async void Btn1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new iseseivtoo());
+        }
+    }
+}
---
+++ LayoutPautova2/LayoutPautova2/iseseivtoo.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace LayoutPautova2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class iseseivtoo : ContentPage
+    {
+    
+        Label lbl, lbl2;
+        Grid grid;
+        public iseseivtoo()
+        {
+            grid = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },                   
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = new GridLength(165) },
+                    new ColumnDefinition { Width = new GridLength(185) }
+                    
+                }
+
+            };
+            //----------------------------------------label 1-----------------------------------------------------
+            lbl = new Label
+
+            {
+                Text = "Количество значений",
+                FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                HorizontalOptions = LayoutOptions.StartAndExpand
+
+            };
+            //-----------------------------------------stepper----------------------------------------------------
+            Stepper stepper = new Stepper
+            {
+                Minimum = 0,
+                Maximum = 50,
+                Increment = 0.5,
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+            };
+            stepper.ValueChanged += Stepper_ValueChanged; ;
+            this.Content = new StackLayout { Children = { lbl, stepper } };
+            grid.Children.Add(lbl, 0, 0);
+            grid.Children.Add(stepper, 0, 1);
+
+
+
+            //-----------------------------------------label 2---------------------------------------------------
+            lbl2 = new Label
+            {
+                Text = "Слайдер",
+                FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                HorizontalOptions = LayoutOptions.EndAndExpand
+            };
+            
+            //----------------------------------------slider-----------------------------------------------------
+      
+            Slider slider = new Slider
+            {
+                Minimum = 0,
+                Maximum = 50,
+                Value = 30
+            };
+
+            slider.ValueChanged += Slider_ValueChanged;
+                 
+            this.Content = new StackLayout { Children = { lbl2, slider } };
+            grid.Children.Add(lbl2, 1, 0);
+            grid.Children.Add(slider, 1, 0);
+
+            Content = grid;
+            grid.BackgroundColor = Color.BurlyWood;
+
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            lbl2.Text = String.Format("Выбрано: {0:F1}", e.NewValue);
+        }
+
+        private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            lbl.Text = String.Format("Выбрано: {0:F1}", e.NewValue);
+        }
+
+        
+
+                
+    }
+           
+};        
